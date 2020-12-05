624276f344cfce0e731dbf3e77a01cc56a1107d3
John Tuckner <tuckner329@gmail.com>
2020-12-05 15:11:55 -0600

    add html example

---
+++ common/website/terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    tines = {
+      source = "github.com/tuckner/tines"
+      version = ">=0.0.5"
+    }
+  }
+}
+
+provider "tines" {
+  email    = var.tines_email
+  base_url = var.tines_base_url
+  token    = var.tines_token
+}
+
+resource "random_id" "webhook_secret" {
+  byte_length = 8
+}
+
+resource "tines_global_resource" "html_template" {
+  name = "html_template"
+  value_type = "text"
+  value = file("${path.module}/resources/index.html")
+}
+
+resource "tines_agent" "webhook" {
+  name = "Webhook Agent"
+  agent_type = "Agents::WebhookAgent"
+  story_id = var.story_id
+  keep_events_for = 604800
+  source_ids = []
+  receiver_ids = []
+  agent_options = jsonencode({
+    "secret": random_id.webhook_secret.dec,
+    "verbs": "get,post",
+    "response_headers": {
+      "content-type": "text/html"
+    },
+    "response": format("{{.RESOURCE.%s}}", tines_global_resource.html_template.name)
+  })
+}
